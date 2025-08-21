e1d2b385825ba70bde2ba2c05d57e9de984fbdf8
Prasanna <Prasannakannan2709@gmail.com>
2025-08-22 00:38:47 +0530

    updated one

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About Us</a></li>
           <li><a href="#programs">Programs</a></li>
-          <li><a href="#admissions">Admissions</a></li>
+          <li><a href="#admission">Admissions</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
